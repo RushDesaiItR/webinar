708c170ef7a286e9d2c2bbad8f0a138230f95487
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2021-04-11 10:07:09 -0700

    ui

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
 })
 
 // START THE SERVER =================================================================
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 7000
 server.listen(port, () => {
   console.log(`Express server listening on port ${port}`)
 })
